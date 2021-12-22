1ed6a39bed043dbfd679645d218fd4c64a43b9cc
Graeme <graeme_c_smith@hotmail.com>
2021-12-22 10:32:20 +0000

    Load docker from local file

--- src/code.sh
+++ src/code.sh
@@ -8,7 +8,7 @@ set -e -x -o pipefail
 ### Set up parameters
 #TODO Check if still needed
 # split project name to get the NGS run number(s)
-run=${echo $project_name |  sed -n 's/^.*_\(NGS.*\)\.*/\1/p'} # Variable not currently used
+run=$(echo $project_name |  sed -n 's/^.*_\(NGS.*\)\.*/\1/p') # Variable not currently used
 
 # Add this if still required
 subpanel_bed_prefix=TODO 
@@ -39,22 +39,22 @@ echo "QC_file = " $QC_file
 # $bamfile_pannumbers is a comma seperated list of pannumbers that should be analysed together.
 # split this into an array and loop through to download BAM and BAI files
 IFS=',' read -ra pannum_array <<<  $bamfile_pannumbers
-for panel in ${pannum_array[@]}
+for panel in ${pannum_array[@]};
 do
-	# check there is at least one bam file with that pan number to download other wise the dx download command will fail
-	if (( $(dx ls $project_name:output/*001.ba* --auth $API_KEY | grep $panel -c) > 0 ));
+	# check there is at least one bam file with that pan number to download otherwise the dx download command will fail
+	num_detected_bams=$(dx ls $project_name:output/*001.ba* --auth $API_KEY | grep -c $panel )
+	if [ num_detected_bams > 0 ] ;
 	then
-		#download all the BAM and BAI files for this project/pan number
+		# download all the BAM and BAI files for this project/pan number
 		dx download $project_name:output/*$panel*001.ba* --auth $API_KEY
+				else
+			echo $panel " related bams not found in " $project_name
 	fi
 done
 
 #Get list of all BAMs 
-bam_list=""
-bam_list=(ls /home/dnanexus/to_test/*bam)
-echo "bam list = " "${bam_list[@]}"
-
-
+bam_list=(/home/dnanexus/to_test/*bam)
+echo "bam list = " ${bam_list[@]}
 
 #TODO Check if still needed
 #count the files. make sure there are at least 3 samples for this pan number, else stop
@@ -77,14 +77,15 @@ mark-section "Run CNV analysis using docker image"
 #Get read count for all samples
 docker load -i '/home/dnanexus/seglh_exomedepth_1220d31.tgz'
 
-for bam in /home/dnanexus/to_test/*bam
+for bam in /home/dnanexus/dnanexus_ED_cnv_calling/to_test/*bam
 #TODO Clean up generation of sample name
+do
 samplename=$(python -c "basename='$bam'; print basename.split('/')[4].split('_R1')[0]")
 echo "samplename:"$samplename
 echo "bam:"$bam
 #for each bam run exomedepth
 # TODO rewrite command and add comment
-docker run -v /home/dnanexus:/home/dnanexus seglh/exomedepth:5f792cb exomeDepth.R 'v1.0.0' /home/dnanexus/out/exomedepth_output/exomedepth_output/"$samplename"_output.pdf $subpanel_bed_path:$subpanel_bed_prefix $readcount_file_path $bam:$samplename $QC_file_path
+docker run -v /home/dnanexus:/home/dnanexus --rm  seglh/exomedepth:1220d31 exomeDepth.R 'v1.0.0' /home/dnanexus/out/exomedepth_output/exomedepth_output/"$samplename"_output.pdf $subpanel_bed_path:$subpanel_bed_prefix $readcount_file_path $bam:$samplename $QC_file_path
 done
 
 # Upload results
