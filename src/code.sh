d87aa38c134b5b2c2454a029555afb01ebe55820
Graeme <graeme_c_smith@hotmail.com>
2022-01-04 15:11:01 +0000

    Expanded README and updated in code comments

--- src/code.sh
+++ src/code.sh
@@ -13,7 +13,6 @@ run=$(echo $project_name |  sed -n 's/^.*_\(NGS.*\)\.*/\1/p')
 subpanel_bed_name=$(dx describe --name "$subpanel_bed")
 readcount_file_name=$(dx describe --name "$readcount_file")
 
-# TODO check if this is required, correctly created
 subpanel_bed_prefix=$(echo "$subpanel_bed_name" | sed -r  's/^[^0-9]*(Pan[0-9]+).*/\1/')
 
 # Location of the ExomeDepth docker file
@@ -49,7 +48,7 @@ mark-section "download bams files and indexes"
 # split this into an array and loop through to download BAM and BAI files
 IFS=',' read -ra pannum_array <<<  $bamfile_pannumbers
 for panel in "${pannum_array[@]}"; do
-	
+ 	
 if [[ " ${pans_from_bams[*]} " =~ " ${panel} " ]]; then
     # If requested pan number has matching bam files
 	dx download "$project_name":output/*"$panel"*001.ba* --auth "$API_KEY"
@@ -64,16 +63,15 @@ echo "bam list = " "${bam_list[@]}"
 
 #count the files. Make sure there are at least 3 samples for this pan number as this is a requirement of the dockerised R script, else stop
 bamfilecount=$(find . -maxdepth 1 -name "*001.bam"  | wc -l)
-baifilecount=$(find . -maxdepth 1 -name "*001.bai"  | wc -l)
-
 if (( bamfilecount < 3 )); then
 	echo "LESS THAN THREE BAM FILES FOUND FOR THIS ANALYSIS" 1>&2
 	exit 1
 fi
 
 # Ensure that every bam file has a bai file
+baifilecount=$(find . -maxdepth 1 -name "*001.bai"  | wc -l)
 if (( baifilecount < bamfilecount )); then
-	echo "ONE OR MORE BAM FILE IS MISSING AN BAI INDEX FILE" 1>&2
+	echo "ONE OR MORE BAM FILE IS MISSING A BAI INDEX FILE" 1>&2
 	exit 1
 fi
 
@@ -98,7 +96,6 @@ samplename=$(basename "$bam" _R1_001.bam)
 echo "samplename:" "$samplename"
 echo "bam:" "$bam"
 echo "subpanel:" "$subpanel_bed_name" 
-#for each bam run exomedepth - the string in the format v1.0.0 will be concatenated to the ouput as the app version
 
 # Handle optional argument relating to QC file
 if [ -z "$QC_file" ]; then
@@ -109,6 +106,7 @@ if [ -z "$QC_file" ]; then
 	bam_and_QC_command="$bam:$samplename $QC_file_path"
 fi
 
+#for each bam run exomedepth - the string in the format v1.0.0 will be concatenated to the ouput as the app version
 docker run -v /home/dnanexus:/home/dnanexus/ \
 	--rm  seglh/exomedepth:1220d31 \
 	exomeDepth.R \
@@ -118,9 +116,6 @@ docker run -v /home/dnanexus:/home/dnanexus/ \
 	/home/dnanexus/in/readcount_file/"$readcount_file_name" \
 	"$bam_and_QC_command"
 done
-
-# 	for debugging run in interactive mode:
-# docker run -v /home/dnanexus:/home/dnanexus/ --rm -it --entrypoint /bin/bash seglh/exomedepth:1220d31
 
 # Upload results
 dx-upload-all-outputs
