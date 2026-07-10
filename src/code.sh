820318c314c266d16a70eb9d15f9113699cc3e61
wintun12 <winmin.tun@nhs.net>
2026-07-10 12:22:18 +0100

    update most recent docker

--- src/code.sh
+++ src/code.sh
@@ -16,7 +16,7 @@ readcount_file_name=$(dx describe --name "$readcount_file")
 subpanel_bed_prefix=$(echo "$subpanel_bed_name" | sed -r  's/^[^0-9]*(Pan[0-9]+).*/\1/')
 
 # Location of the ExomeDepth docker file
-docker_file_id=project-J32193pK9yGfjP2GyZ94KZf4:file-J96K4BBK9yGgbJ74fqzPbGqv
+docker_file_id=project-J32193pK9yGfjP2GyZ94KZf4:file-J98J01pK9yGv407fX40GZVFB
 
 #read the DNA Nexus api key as a variable
 API_KEY_wquotes=$(echo $DX_SECURITY_CONTEXT |  jq '.auth_token')
